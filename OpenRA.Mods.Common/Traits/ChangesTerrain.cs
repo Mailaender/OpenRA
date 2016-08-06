b5e448f9dd578e06c42bf7652973b5ca07b0fd3d
Matthias Mailänder <matthias@mailaender.name>
2016-08-06 14:45:02 +0200

    Add ChangesTerrain.

---
+++ OpenRA.Mods.Common/Traits/ChangesTerrain.cs
@@ -0,0 +1,41 @@
+﻿#region Copyright & License Information
+/*
+ * Copyright 2007-2016 The OpenRA Developers (see AUTHORS)
+ * This file is part of OpenRA, which is free software. It is made
+ * available to you under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version. For more
+ * information, see COPYING.
+ */
+#endregion
+
+using OpenRA.Traits;
+
+namespace OpenRA.Mods.Common.Traits
+{
+	[Desc("Modifies the terrain type underneath the actors location.")]
+	class ChangesTerrainInfo : ITraitInfo
+	{
+		[FieldLoader.Require] public readonly string TerrainType = null;
+
+		public object Create(ActorInitializer init) { return new ChangesTerrain(this); }
+	}
+
+	class ChangesTerrain : INotifyAddedToWorld
+	{
+		readonly ChangesTerrainInfo info;
+
+		public ChangesTerrain(ChangesTerrainInfo info)
+		{
+			this.info = info;
+		}
+
+		void INotifyAddedToWorld.AddedToWorld(Actor self)
+		{
+			var cell = self.Location;
+			var map = self.World.Map;
+			var terrain = map.Rules.TileSet.GetTerrainIndex(info.TerrainType);
+			map.CustomTerrain[cell] = terrain;
+		}
+	}
+}
