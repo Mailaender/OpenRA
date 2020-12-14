3f40dc83d2991b77bcbefb4a661cdf65c41cfbaa
Matthias Mailänder <matthias@mailaender.name>
2020-12-14 22:27:28 +0100

    Add support for Westwood ADPCM Audio.

--- OpenRA.Mods.Cnc/FileFormats/AudReader.cs
+++ OpenRA.Mods.Cnc/FileFormats/AudReader.cs
@@ -71,6 +71,7 @@ namespace OpenRA.Mods.Cnc.FileFormats
 				channels = readFlag == (int)SoundFlags.Stereo ? 2 : 1;
 
 				var readFormat = s.ReadByte();
+				System.Console.WriteLine(readFormat);
 				if (!Enum.IsDefined(typeof(SoundFormat), readFormat))
 					return false;
 
@@ -79,7 +80,10 @@ namespace OpenRA.Mods.Cnc.FileFormats
 				result = () =>
 				{
 					var audioStream = SegmentStream.CreateWithoutOwningStream(s, offsetPosition, (int)(s.Length - offsetPosition));
-					return new AudStream(audioStream, outputSize, dataSize);
+					if (readFormat == (int)SoundFormat.ImaAdpcm)
+						return new AudStream(audioStream, outputSize, dataSize);
+					else
+						return new WestwoodAudStream(audioStream, outputSize, dataSize);
 				};
 			}
 			finally
@@ -139,6 +143,129 @@ namespace OpenRA.Mods.Cnc.FileFormats
 				dataSize -= 8 + chunk.CompressedSize;
 
 				return dataSize <= 0;
+			}
+		}
+
+		sealed class WestwoodAudStream : ReadOnlyAdapterStream
+		{
+			readonly int outputSize;
+			readonly int dataSize;
+
+			static readonly short[] WSTable2bit = { -2, -1, 0, 1 };
+			static readonly short[] WSTable4bit =
+			{
+				-9, -8, -6, -5, -4, -3, -2, -1,
+				0,  1,  2,  3,  4,  5,  6,  8
+			};
+
+			public WestwoodAudStream(Stream stream, int outputSize, int dataSize)
+				: base(stream)
+			{
+				this.outputSize = outputSize;
+				this.dataSize = dataSize;
+
+				System.Console.WriteLine(outputSize + " " + dataSize);
+			}
+
+			public override long Length
+			{
+				get { return outputSize; }
+			}
+
+			protected override bool BufferData(Stream baseStream, Queue<byte> data)
+			{
+				if (dataSize <= 0)
+					return true;
+
+				if (dataSize == outputSize)
+				{
+					data.Append(baseStream.ReadAllBytes());
+					return true;
+				}
+
+				var sample = 0x80;
+
+				while (baseStream.Position + 16 < baseStream.Length)
+				{
+					var input = baseStream.ReadUInt8();
+					input <<= 2;
+					var command = (byte)(input >> 8);
+					var count = (byte)((input & 0xff) >> 2);
+
+					System.Console.WriteLine("dataSize: " + dataSize + " baseStream.Position " + baseStream.Position);
+					System.Console.WriteLine("count " + count);
+					switch (command)
+					{
+						case 2: // no compression
+						if ((count & 0x20) != 0)
+						{
+							count <<= 3;
+							sample += count >> 3;
+							data.Enqueue((byte)sample);
+						}
+						else
+						{
+							for (var i = 0; i < count; i++)
+								data.Enqueue(baseStream.ReadUInt8());
+
+							baseStream.Position -= 1;
+							sample = baseStream.ReadUInt8();
+							sample &= 0xffff;
+						}
+
+						break;
+
+						case 1: // ADPCM 8-bit -> 4-bit
+						for (count++; count > 0; count--)
+						{
+							command = baseStream.ReadUInt8();
+
+							sample += WSTable4bit[command & 0x0F]; // lower nibble
+
+							sample = sample.Clamp(0, 255);
+							data.Enqueue((byte)sample);
+
+							sample += WSTable4bit[command >> 4]; // higher nibble
+							sample = sample.Clamp(0, 255);
+							data.Enqueue((byte)sample);
+						}
+
+						break;
+
+						case 0: // ADPCM 8-bit -> 2-bit
+						for (count++; count > 0; count--)
+						{
+							command = baseStream.ReadUInt8();
+
+							sample += WSTable2bit[command & 0x03]; // lower 2 bits
+							sample = sample.Clamp(0, 255);
+							data.Enqueue((byte)sample);
+
+							sample += WSTable2bit[(command >> 2) & 0x03]; // lower middle 2 bits
+							sample = sample.Clamp(0, 255);
+							data.Enqueue((byte)sample);
+
+							sample += WSTable2bit[(command >> 4) & 0x03]; // higher middle 2 bits
+							sample.Clamp(0, 255);
+							data.Enqueue((byte)sample);
+
+							sample += WSTable2bit[(command >> 6) & 0x03]; // higher 2 bits
+							sample = sample.Clamp(0, 255);
+							data.Enqueue((byte)sample);
+						}
+
+						break;
+
+						default:
+						for (count++; count > 0; count--)
+							data.Enqueue((byte)sample);
+						break;
+					}
+				}
+
+				//data = data.Reverse();
+
+				return true;
 			}
 		}
 	}
