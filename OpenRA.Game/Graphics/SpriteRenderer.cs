f4d2eb100a5662aaf3d6fac29ec22707b232722d
Paul Chote <pchote@users.noreply.github.com>
2020-12-29 12:03:47 +0000

    Support multiple sheets in TerrainSpriteLayer.

--- OpenRA.Game/Graphics/SpriteRenderer.cs
+++ OpenRA.Game/Graphics/SpriteRenderer.cs
@@ -10,6 +10,7 @@
 #endregion
 
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using OpenRA.Primitives;
 
@@ -17,7 +18,7 @@ namespace OpenRA.Graphics
 {
 	public class SpriteRenderer : Renderer.IBatchRenderer
 	{
-		const int SheetCount = 7;
+		public const int SheetCount = 7;
 		static readonly string[] SheetIndexToTextureName = Exts.MakeArray(SheetCount, i => "Texture{0}".F(i));
 
 		readonly Renderer renderer;
@@ -151,9 +152,18 @@ namespace OpenRA.Graphics
 			nv += 6;
 		}
 
-		public void DrawVertexBuffer(IVertexBuffer<Vertex> buffer, int start, int length, PrimitiveType type, Sheet sheet, BlendMode blendMode)
+		public void DrawVertexBuffer(IVertexBuffer<Vertex> buffer, int start, int length, PrimitiveType type, IEnumerable<Sheet> sheets, BlendMode blendMode)
 		{
-			shader.SetTexture("Texture0", sheet.GetTexture());
+			var i = 0;
+			foreach (var s in sheets)
+			{
+				if (i >= SheetCount)
+					throw new ArgumentException("SpriteRenderer only supports {0} simultaneous textures".F(SheetCount), nameof(sheets));
+
+				if (s != null)
+					shader.SetTexture(SheetIndexToTextureName[i++], s.GetTexture());
+			}
+
 			renderer.Context.SetBlendMode(blendMode);
 			shader.PrepareRender();
 			renderer.DrawBatch(buffer, start, length, type);
