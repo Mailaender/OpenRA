f07de29455d629337550cb3188757d5d09177480
penev92 <penev92@gmail.com>
2021-02-17 13:07:47 +0200

    Refactored GrantChargeDrainCondition trait
    Added more flexibility to its TraitInfo via additional settings, made the TimedDeployState enum private, added a charge points system (for both improved flexibility for modders and simplified calculations), added a CanDeploy() method (in order to centralize voice/cursor/(de)activation availability checks) and removed the SyncAttribute from the deployState field because apparently we can't sync enum fields.

--- OpenRA.Mods.Common/Traits/Conditions/GrantChargeDrainCondition.cs
+++ OpenRA.Mods.Common/Traits/Conditions/GrantChargeDrainCondition.cs
@@ -21,26 +21,37 @@ namespace OpenRA.Mods.Common.Traits
 		"by undeploying. Leftover charges are taken into account when recharging.")]
 	public class GrantChargeDrainConditionInfo : PausableConditionalTraitInfo
 	{
-		[GrantedConditionReference]
 		[FieldLoader.Require]
+		[Desc("Amount of charge points the actor can build up to.")]
+		public readonly int MaxChargePoints = 0;
+
+		[FieldLoader.Require]
+		[Desc("Time it takes to charge up one charge point.")]
+		public readonly int ChargeTicksPerPoint = 0;
+
+		[FieldLoader.Require]
+		[Desc("Time it takes to discharge one charge point.")]
+		public readonly int DrainTicksPerPoint = 0;
+
+		[Desc("How many charge points does the actor spawn with.")]
+		public readonly int StartingChargePoints = 0;
+
+		[Desc("Minimum number of charge points required to activate the condition.")]
+		public readonly int MinActivationPoints = 0;
+
+		[Desc("Whether the player can manually disable the condition once activated before the trait fully discharges.")]
+		public readonly bool CanCancelCondition = false;
+
+		[FieldLoader.Require]
+		[GrantedConditionReference]
 		[Desc("The condition granted after deploying.")]
 		public readonly string DeployedCondition = null;
-
-		[FieldLoader.Require]
-		[Desc("Time it takes to fully charge up again.")]
-		public readonly int ChargeTicks = 0;
-
-		[FieldLoader.Require]
-		[Desc("The deployed state's length in ticks.")]
-		public readonly int DrainTicks = 0;
 
 		[Desc("Cursor to display when able to (un)deploy the actor.")]
 		public readonly string DeployCursor = "deploy";
 
 		[Desc("Cursor to display when unable to (un)deploy the actor.")]
 		public readonly string DeployBlockedCursor = "deploy-blocked";
-
-		public readonly bool StartsFullyCharged = false;
 
 		[VoiceReference]
 		public readonly string Voice = "Action";
@@ -52,19 +63,21 @@ namespace OpenRA.Mods.Common.Traits
 		public override object Create(ActorInitializer init) { return new GrantChargeDrainCondition(init.Self, this); }
 	}
 
-	public enum TimedDeployState { Charging, Ready, Active, Deploying, Undeploying }
-
 	public class GrantChargeDrainCondition : PausableConditionalTrait<GrantChargeDrainConditionInfo>,
 		IResolveOrder, IIssueOrder, ISelectionBar, IOrderVoice, ISync, ITick, IIssueDeployOrder
 	{
+		enum TimedDeployState { Charging, Ready, Active, Deploying, Undeploying }
+
 		readonly Actor self;
 
 		int deployedToken = Actor.InvalidConditionToken;
 
 		[Sync]
-		int ticks;
+		int currentChargePoints;
 
 		[Sync]
+		int ticks;
+
 		TimedDeployState deployState;
 
 		public GrantChargeDrainCondition(Actor self, GrantChargeDrainConditionInfo info)
@@ -75,16 +88,9 @@ namespace OpenRA.Mods.Common.Traits
 
 		protected override void Created(Actor self)
 		{
-			if (Info.StartsFullyCharged)
-			{
-				ticks = Info.DrainTicks;
-				deployState = TimedDeployState.Ready;
-			}
-			else
-			{
-				ticks = Info.ChargeTicks;
-				deployState = TimedDeployState.Charging;
-			}
+			currentChargePoints = Info.StartingChargePoints;
+			deployState = currentChargePoints == Info.MaxChargePoints ? TimedDeployState.Ready : TimedDeployState.Charging;
+			ticks = deployState == TimedDeployState.Charging ? Info.ChargeTicksPerPoint : Info.DrainTicksPerPoint;
 
 			base.Created(self);
 		}
@@ -94,7 +100,10 @@ namespace OpenRA.Mods.Common.Traits
 			return new Order("GrantChargeDrainCondition", self, queued);
 		}
 
-		bool IIssueDeployOrder.CanIssueDeployOrder(Actor self, bool queued) { return !IsTraitPaused && !IsTraitDisabled; }
+		bool IIssueDeployOrder.CanIssueDeployOrder(Actor self, bool queued)
+		{
+			return CanDeploy();
+		}
 
 		IEnumerable<IOrderTargeter> IIssueOrder.Orders
 		{
@@ -108,15 +117,12 @@ namespace OpenRA.Mods.Common.Traits
 
 		Order IIssueOrder.IssueOrder(Actor self, IOrderTargeter order, in Target target, bool queued)
 		{
-			if (order.OrderID == "GrantChargeDrainCondition")
-				return new Order(order.OrderID, self, queued);
-
-			return null;
+			return order.OrderID == "GrantChargeDrainCondition" ? new Order(order.OrderID, self, queued) : null;
 		}
 
 		void IResolveOrder.ResolveOrder(Actor self, Order order)
 		{
-			if (order.OrderString != "GrantChargeDrainCondition")
+			if (order.OrderString != "GrantChargeDrainCondition" || !CanDeploy())
 				return;
 
 			if (!order.Queued)
@@ -130,12 +136,29 @@ namespace OpenRA.Mods.Common.Traits
 
 		bool IsCursorBlocked()
 		{
-			return IsTraitPaused;
+			return !CanDeploy();
 		}
 
 		string IOrderVoice.VoicePhraseForOrder(Actor self, Order order)
 		{
-			return order.OrderString == "GrantChargeDrainCondition" ? Info.Voice : null;
+			return order.OrderString == "GrantChargeDrainCondition" && CanDeploy() ? Info.Voice : null;
+		}
+
+		bool CanDeploy()
+		{
+			if (IsTraitPaused || IsTraitDisabled)
+				return false;
+
+			if (deployState == TimedDeployState.Charging && currentChargePoints < Info.MinActivationPoints)
+				return false;
+
+			if (deployState == TimedDeployState.Active && !Info.CanCancelCondition)
+				return false;
+
+			if (deployState == TimedDeployState.Deploying || deployState == TimedDeployState.Undeploying)
+				return false;
+
+			return true;
 		}
 
 		void Deploy()
@@ -149,9 +172,6 @@ namespace OpenRA.Mods.Common.Traits
 		{
 			if (deployedToken == Actor.InvalidConditionToken)
 				deployedToken = self.GrantCondition(Info.DeployedCondition);
-
-			var drainPercentage = 100 - (ticks * 100 / Info.ChargeTicks);
-			ticks = Info.DrainTicks * drainPercentage / 100;
 
 			deployState = TimedDeployState.Active;
 		}
@@ -169,9 +189,6 @@ namespace OpenRA.Mods.Common.Traits
 				deployedToken = self.RevokeCondition(deployedToken);
 
 			deployState = TimedDeployState.Charging;
-
-			var chargePercentage = 100 - (ticks * 100 / Info.DrainTicks);
-			ticks = Info.ChargeTicks * chargePercentage / 100;
 		}
 
 		void ITick.Tick(Actor self)
@@ -182,33 +199,43 @@ namespace OpenRA.Mods.Common.Traits
 			if (deployState == TimedDeployState.Ready || deployState == TimedDeployState.Deploying || deployState == TimedDeployState.Undeploying)
 				return;
 
-			if (--ticks < 1)
+			if (--ticks != 0)
+				return;
+
+			if (deployState == TimedDeployState.Charging)
 			{
-				if (deployState == TimedDeployState.Charging)
+				currentChargePoints++;
+				if (currentChargePoints == Info.MaxChargePoints)
 				{
-					ticks = Info.DrainTicks;
 					deployState = TimedDeployState.Ready;
+					ticks = Info.DrainTicksPerPoint;
 				}
 				else
+					ticks = Info.ChargeTicksPerPoint;
+			}
+			else
+			{
+				currentChargePoints--;
+				if (currentChargePoints == 0)
+				{
 					RevokeDeploy();
+					ticks = Info.ChargeTicksPerPoint;
+				}
+				else
+					ticks = Info.DrainTicksPerPoint;
 			}
 		}
 
 		float ISelectionBar.GetValue()
 		{
-			if (IsTraitDisabled || !Info.ShowSelectionBar || deployState == TimedDeployState.Undeploying)
+			if (IsTraitDisabled || !Info.ShowSelectionBar)
 				return 0f;
 
-			if (deployState == TimedDeployState.Deploying || deployState == TimedDeployState.Ready)
-				return 1f;
-
-			return deployState == TimedDeployState.Charging
-				? (float)(Info.ChargeTicks - ticks) / Info.ChargeTicks
-				: (float)ticks / Info.DrainTicks;
+			return (float)currentChargePoints / Info.MaxChargePoints;
 		}
 
-		bool ISelectionBar.DisplayWhenEmpty { get { return !IsTraitDisabled && Info.ShowSelectionBar; } }
+		bool ISelectionBar.DisplayWhenEmpty => !IsTraitDisabled && Info.ShowSelectionBar;
 
-		Color ISelectionBar.GetColor() { return deployState == TimedDeployState.Charging ? Info.ChargingColor : Info.DischargingColor; }
+		Color ISelectionBar.GetColor() => deployState == TimedDeployState.Charging ? Info.ChargingColor : Info.DischargingColor;
 	}
 }
